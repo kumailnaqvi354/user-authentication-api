2194f4a63b5cf330a1aa9a14e6b2e48f60b5ea3d
Kumail Naqvi <57100826+kumailnaqvi354@users.noreply.github.com>
2021-04-13 18:45:15 +0500

    register user bug removed

--- config/custom.js
+++ config/custom.js
@@ -10,7 +10,7 @@
 
 module.exports.custom = {
 
-  baseUrl: 'http://localhost:1337',
+  // baseUrl: 'http://localhost:1337',
 
   emailProofTokenTTL: 24 * 60 * 60 * 1000, // 24 hours
 
@@ -20,7 +20,7 @@ module.exports.custom = {
   * Any other custom config this Sails app should use during development.    *
   *                                                                          *
   ***************************************************************************/
-  // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
+  // sendgridSecret: "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",
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
 
